5323e078e5e9eba89fe4f6bfa1069f0e2f792399
youzhen <yl301114@gmail.com>
2024-08-12 08:56:24 +0100

    mail

--- public/js/index.js
+++ public/js/index.js
@@ -11,18 +11,39 @@ $(".btn-top").click(function(){
     $("html, body").animate({scrollTop: 0},300);
 })
 
+var swiperFloor = new Swiper(".floor_swiper", {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 800,
+})
+
+var swiperMaterial = new Swiper(".material_swiper", {
+    slidesPerView: 1,
+    effect: "fade",
+    speed: 800,
+    allowTouchMove: false,
+})
+$(".material_brand_item").click(function(){
+    $(this).addClass("active").siblings(".material_brand_item").removeClass("active");
+    let li_index = $(this).index();
+    swiperMaterial.slideTo(li_index, 1000, true);
+})
 
 
 function sendEmail(){
     Email.send({
         SecureToken: "8207c7d7-4a6c-4797-870d-d16ee71023ce",
-        To : "zhulixdesign@gmail.com, zhuleservice@gmail.com, spdnbhj@gmail.com",
+        // To : "zhulixdesign@gmail.com, zhuleservice@gmail.com, spdnbhj@gmail.com",
+        To : "yl301114@gmail.com",
         From : "walayydesign@gmail.com",
-        Subject : "玖都預約賞屋",
-        Body : "貴賓稱謂:" + document.getElementById("name").value
-                + "<br>聯絡電話:" + document.getElementById("phone").value
-                + "<br>Email信箱:" + document.getElementById("email").value
-                + "<br>聯繫時段:" + document.getElementById("time_from").value + "時-" + document.getElementById("time_to").value + "時"
+        Subject : "平鎮源邸預約賞屋",
+        Body : "姓名:" + document.getElementById("name").value
+                + "<br>手機:" + document.getElementById("phone").value
+                + "<br>居住地區:" + document.getElementById("area").value
+                + "<br>留言:" + document.getElementById("remark").value
     }).then(
         message => alert("感謝您的來信！我們很快就會和您聯繫！"),
     );
