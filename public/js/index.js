06d4e2e27e8bd1856b7f9e47cf1c8c6f1814d8d0
youzhen <yl301114@gmail.com>
2024-08-13 19:14:54 +0100

    修改

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
-// AOS.init({startEvent: 'load'});
-// window.addEventListener('load', AOS.refresh);
+AOS.init({startEvent: 'load'});
+window.addEventListener('load', AOS.refresh);
+
+
+function sendEmail(){
+    Email.send({
+        SecureToken: "8207c7d7-4a6c-4797-870d-d16ee71023ce",
+        To : "yl30831114@gmail.com",
+        From : "walayydesign@gmail.com",
+        Subject : "平鎮源邸預約賞屋",
+        Body : "!!!",
+        // Body : "姓名:" + document.getElementById("name").value
+        //         + "<br>手機:" + document.getElementById("phone").value
+        //         + "<br>居住地區:" + document.getElementById("area").value
+        //         + "<br>留言:" + document.getElementById("remark").value
+    }).then(
+        message => alert("感謝您的來信！我們很快就會和您聯繫！"),
+    );
+}
 
 $(".header_item").click(function(){
     let target = $(this).data("target");
@@ -32,19 +49,3 @@ $(".material_brand_item").click(function(){
     swiperMaterial.slideTo(li_index, 1000, true);
 })
 
-
-function sendEmail(){
-    Email.send({
-        SecureToken: "8207c7d7-4a6c-4797-870d-d16ee71023ce",
-        // To : "zhulixdesign@gmail.com, zhuleservice@gmail.com, spdnbhj@gmail.com",
-        To : "yl301114@gmail.com",
-        From : "walayydesign@gmail.com",
-        Subject : "平鎮源邸預約賞屋",
-        Body : "姓名:" + document.getElementById("name").value
-                + "<br>手機:" + document.getElementById("phone").value
-                + "<br>居住地區:" + document.getElementById("area").value
-                + "<br>留言:" + document.getElementById("remark").value
-    }).then(
-        message => alert("感謝您的來信！我們很快就會和您聯繫！"),
-    );
-}
